b26f7a7c7a68b883fcff90dff1523c2579d5a1c1
Daniel Józefiuk <daniel.jozefiuk@bcoders.pl>
2021-10-08 14:19:50 +0200

    SignalR added and configured

---
+++ API/Controllers/ChartController.cs
@@ -0,0 +1,31 @@
+﻿using API.DataStorage;
+using API.HubConfig;
+using API.TimerFeatures;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartController : ControllerBase
+    {
+        private IHubContext<ChartHub> _hub;
+        
+        public ChartController(IHubContext<ChartHub> hub)
+        {
+            _hub = hub;
+        }
+
+        public IActionResult Get()
+        {
+            var timeManager = new TimerManager(() => _hub.Clients.All.SendAsync("transferchartdata", DataManager.GetData()));
+            return Ok(new { Message = "Request Completed" });
+        }
+    }
+}
